008b4794472397d74e9ade91beace12772e10609
Nikita Gore <nikitagore@wirelessprv-10-193-81-230.near.illinois.edu>
2022-06-15 21:44:17 -0500

    login update

--- login.py
+++ login.py
@@ -1,12 +1,16 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
 
 
 class MainApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "BlueGray"
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Red"
         return Builder.load_file('login.kv')
-
 
 MainApp().run()
